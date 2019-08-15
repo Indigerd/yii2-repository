040d503d8ff8c760ac7de47ac647004dbad1b05e
indigerd <alex.stepanenko@gmail.com>
2019-08-15 19:01:59 +0300

    added MongoDB QueryBuilder

---
+++ src/Query/MongoQueryBuilder.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Indigerd\Repository\Query;
+
+use yii\mongodb\Query;
+use yii\db\QueryInterface;
+use Indigerd\Repository\Exception\DeleteException;
+use Indigerd\Repository\Exception\UpdateException;
+
+class MongoQueryBuilder extends AbstractQueryBuilder
+{
+    protected function createQuery(): QueryInterface
+    {
+        return new Query();
+    }
+
+    public function queryOne(array $conditions, array $relations = []): ?array
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        $query
+            ->from($this->collectionName)
+            ->where($conditions);
+
+        return $query->one($this->connection);
+    }
+
+    public function insert(array $data): ?array
+    {
+        /** @var \yii\mongodb\Connection $connection */
+        $connection = $this->connection;
+        $newId = $connection->getCollection($this->collectionName)->insert($data);
+        return ['_id' => $newId];
+    }
+
+    public function updateOne(array $data): void
+    {
+        if (empty($data['_id'])) {
+            throw new UpdateException($data, "Primary key _id not provided");
+        }
+        $conditions = ['_id' => $data['_id']];
+        unset($data['_id']);
+        $this->updateAll($data, $conditions);
+    }
+
+
+    public function updateAll(array $data, array $conditions): int
+    {
+        /** @var \yii\mongodb\Connection $connection */
+        $connection = $this->connection;
+        return $connection->getCollection($this->collectionName)->update($conditions, $data);
+    }
+
+    public function deleteOne(array $data): void
+    {
+        if (empty($data['_id'])) {
+            throw new DeleteException($data, "Primary key _id not provided");
+        }
+        $conditions = ['_id' => $data['_id']];
+        $this->deleteAll($conditions);
+
+    }
+
+    public function deleteAll(array $conditions): int
+    {
+        /** @var \yii\mongodb\Connection $connection */
+        $connection = $this->connection;
+        $connection->getCollection($this->collectionName)->remove($conditions);
+    }
+
+    public function queryAll(array $conditions, array $order = [], int $limit = 0, int $offset = 0, array $relations = []): array
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        $query
+            ->from($this->collectionName)
+            ->where($conditions);
+
+        if ($limit > 0) {
+            $query->limit($limit)->offset($offset);
+        }
+
+        if (!empty($order)) {
+            $query->orderBy($order);
+        }
+
+        return $query->all($this->connection);
+    }
+
+    public function aggregate(string $expression, array $conditions): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->select($expression)->where($conditions)->scalar($this->connection);
+    }
+
+    public function aggregateCount(string $field = '', array $conditions = []): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->where($conditions)->count('*', $this->connection);
+    }
+
+    public function aggregateSum(string $field, array $conditions = []): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->where($conditions)->sum($field, $this->connection);
+    }
+
+    public function aggregateAverage(string $field, array $conditions = []): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->where($conditions)->average($field, $this->connection);
+    }
+
+    public function aggregateMin(string $field, array $conditions = []): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->where($conditions)->min($field, $this->connection);
+    }
+
+    public function aggregateMax(string $field, array $conditions = []): string
+    {
+        /** @var Query $query */
+        $query = $this->createQuery();
+        return (string)$query->where($conditions)->max($field, $this->connection);
+    }
+}
